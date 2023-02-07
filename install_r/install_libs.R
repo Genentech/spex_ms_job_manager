74aa62f2270e2f8dfab927d41f90e71d27a18755
Aleksandr Prilipko <zverozabr@yandex.ru>
2023-02-07 11:02:24 +0700

    changed libs install script

--- install_r/install_libs.R
+++ install_r/install_libs.R
@@ -24,7 +24,7 @@ if(length(not_installed) > 0){
   pacman::p_install(not_installed,lib = .libPaths()[1],dependencies = TRUE, verify_installed = TRUE, quiet = TRUE)
 }
 
-packages_to_check <- c("FNN", "RANN")
+packages_to_check <- c("FNN", "RANN", "RJSONIO")
 not_installed <- packages_to_check[!packages_to_check %in% rownames(installed.packages())]
 
 for (pkg in not_installed) {
